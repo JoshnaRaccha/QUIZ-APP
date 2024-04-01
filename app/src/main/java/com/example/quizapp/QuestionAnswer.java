39c937cbbab9fa06ce99afa17268ada70bd721a2
Joshna Rachha <joshnarachha@gmail.com>
2024-04-01 20:59:53 +0530

    quiz app

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is 15+26 ?",
+            "What is 20/5",
+            "what is 12*9 ?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"32" , "41" , "36" , "38"},
+            {"6" , "8" , "7" , "4"},
+            {"96" , "84" , "102" , "108"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "41" ,
+            "4",
+            "108",
+            "Elon Musk",
+            "All of the above"};
+}
